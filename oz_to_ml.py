1e45f5be2b1e84495f1609519f7dcf1874aec651
ArminAwista <armin.awista23@ycdsbk12.ca>
2020-12-03 19:01:49 +0000

    ounce

---
+++ oz_to_ml.py
@@ -0,0 +1,6 @@
+ounce1 = float(input("Enter the amount of fluid in ounces: "))
+serving1 = float(input("Enter the number of people to serve: "))
+
+millimeters1 = (ounce1 / 4)* 29.57
+
+print("You will need " + str(millimeters1) + " millimeters.")
