271573a9286aadf0c0d6838422ab9a3dd48ba7d9
ArminAwista <armin.awista23@ycdsbk12.ca>
2020-12-03 17:55:55 +0000

    Changed.

---
+++ avg_marks.py
@@ -0,0 +1,8 @@
+mark1 = float(input("Enter mark 1: "))
+mark2 = float(input("Enter mark 2: "))
+mark3 = float(input("Enter mark 3: "))
+mark4 = float(input("Enter mark 4: "))
+
+marks_avg = (mark1 + mark2 + mark3 + mark4)/4
+
+print("The average of the 4 marks is " + str(marks_avg))
